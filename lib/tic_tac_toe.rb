e8f4720e38679750c318e8c14d3888e32cd348d2
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-19 15:09:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,3 +9,55 @@ WIN_COMBINATIONS = [
   [0,4,8], # top l to bottom r diagonal
   [2,4,6] # top r to bottom l diagonal
 ]
+
+# Boolean Method for determining if a game has been won
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return win_combination
+    end
+  end
+  return false
+end
+
+# Boolean Method for determining if the board is full
+def full?(board)
+  board.all? {|i| i == "X" || i == "O"}
+end
+
+# Boolean Method for determining if the game is a draw
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else if !won?(board) && !full?(board)
+    return false
+  else won?(board)
+    return false
+  end
+end
+end
+
+
+# Boolean Method for determining if the game is over
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+# Method for determining/outputting the winner
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
