8581660d99be7ec121415ced5351bc0869ca9954
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-19 15:45:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,16 +58,7 @@ end
 
 # Returns the current player
 
-def current_player(board)
-  if turn_count(board).even? == true
-    return "X"
-  else
-    return "O"
-  end
-    puts "Turns played: #{counter}"
-  end
-  end
-end
+
 
 # Asks for user's input, makes valid moves and updates the board, or asks for new input if the move is invalid.
 def turn(board)
